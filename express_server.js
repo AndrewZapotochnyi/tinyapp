55fd0b262d958a9eebd0556a172ccde43785cd38
AndrewZapotochnyi <info@azblockchain.co>
2020-04-13 22:01:01 +0000

    Tiny URL - added header

--- express_server.js
+++ express_server.js
@@ -17,6 +17,11 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
